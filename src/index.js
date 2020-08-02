d31c91ce24470c6cd36d8888b371fb7f8a5f38e9
ahncarolina <ahncarolina@gmail.com>
2020-08-02 10:53:31 -0300

    ops: implement Carousel slider

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 import Home from "./pages/home";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CadastroVideo from "./pages/cadastro/video";
+import CadastroCategoria from "./pages/cadastro/categoria";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Home} />
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={() => <div>Página 404</div>} />
     </Switch>
   </BrowserRouter>,
